a9a56da7782f46db9f8a976cf5324b0b319e2aa4
T.J.K. <tjk16384@gmail.com>
2013-08-19 13:45:24 -0400

    added button linking to main menu

--- HelloWorld/hw.js
+++ HelloWorld/hw.js
@@ -1,15 +1,11 @@
-var BG = new collie.Layer({
-    width: 640,
-    height: 480
-});
-var Layer1 = new collie.Layer({
-    width: 640,
-    height: 480
-});
-var TitleScreen = new collie.Layer({
-    width: 640,
-    height: 480
-});
+var W = 720;
+var H = 540;
+
+var BG = new collie.Layer({ width: W, height: H });
+var Layer1 = new collie.Layer({ width: W, height: H });
+var TitleScreen = new collie.Layer({ width: W, height: H });
+var MainMenu = new collie.Layer({ width: W, height: H });
+
 
 collie.ImageManager.add({
     logo: "http://jindo.dev.naver.com/collie/img/small/logo.png",
@@ -20,18 +16,20 @@ collie.ImageManager.add({
     Munchkin: "./super_munchkin.png"
 });
 
-var oBG = new collie.DisplayObject({
+
+var oBGPaper = new collie.DisplayObject({
     x: "center",
     y: "center",
-    width: 640,
-    height: 480,
+    width: W,
+    height: H,
     backgroundImage: "Paper"
 }).addTo(BG);
+
 
 var oGround = new collie.DisplayObject({
     x: 0,
     y: "bottom",
-    width: 640 * 2,
+    width: W * 2,
     height: 88,
     backgroundImage: "ground",
     backgroundRepeat: "repeat-x",
@@ -54,7 +52,7 @@ var oLoadText = new collie.Text({
     y: "center",
     width: 400,
     textAlign: "center",
-    fontFamily: "Quasimodo",
+    fontFamily: "Quasi",
     fontColor: "rgb(64,32,0)",
     fontSize: 64
 }).text("Loading . . .").addTo(Layer1);
@@ -69,53 +67,85 @@ collie.Timer.cycle(oYoshi, 750, {
 var oTitle = new collie.Text({
     x: "center",
     y: 32,
-    width: 640,
-    scaleX: 0.9,
+    width: W,
+    scaleX: 1,
     textAlign: "center",
-    fontFamily: "Quasimodo",
+    fontFamily: "Quasi",
     fontColor: "rgb(64,32,0)",
-    fontSize: 96
+    fontSize: 100
 }).text("Munchkin").addTo(TitleScreen);
 
 var oTitle1 = new collie.Text({
-    x: 280,
-    y: 128,
-    width: 320,
+    x: 288,
+    y: 144,
+    width: 372,
     textAlign: "center",
-    fontFamily: "Quasimodo",
+    fontFamily: "Quasi",
     fontColor: "rgb(64,32,0)",
-    fontSize: 24
+    fontSize: 28
 }).text("Original Game Design by Steve Jackson").addTo(TitleScreen);
 
 var oTitle2 = new collie.Text({
-    x: 336,
-    y: 192,
-    width: 224,
+    x: 352,
+    y: 224,
+    width: 256,
     textAlign: "center",
-    fontFamily: "Quasimodo",
+    fontFamily: "Quasi",
     fontColor: "rgb(64,32,0)",
-    fontSize: 24
+    fontSize: 28
 }).text("Illustrations by John Kovalic").addTo(TitleScreen);
 
 var oTitle3 = new collie.Text({
     x: 280,
-    y: 256,
-    width: 336,
+    y: 304,
+    width: 400,
     textAlign: "center",
-    fontFamily: "Quasimodo",
+    fontFamily: "Quasi",
     fontColor: "rgb(64,32,0)",
-    fontSize: 24
+    fontSize: 28
 }).text("JavaScript Port designed + coded by Dustin Duffy, Tom Kruk + John Prinz").addTo(TitleScreen);
 
 var oMunchkin = new collie.DisplayObject({
-    x: -24,
-    y: 72,
+    x: -8,
+    y: 108,
     width: 360,
     height: 440,
-    scaleX: 0.8,
-    scaleY: 0.8,
+    scaleX: 0.9,
+    scaleY: 0.9,
     backgroundImage: "Munchkin"
 }).addTo(TitleScreen);
+
+var oBtnStart = new collie.DisplayObject({
+    x: 384,
+    y: 432,
+    width: 256,
+    height: 64,
+    backgroundColor: "rgba(255,255,192,0.6)"
+}).attach({
+    "click": showMainMenu
+}).addTo(TitleScreen);
+
+var oBtnStartText = new collie.Text({
+    x: 384,
+    y: 448,
+    width: 256,
+    //height: 64,
+    textAlign: "center",
+    fontFamily: "Quasi",
+    fontColor: "rgb(64,32,0)",
+    fontSize: 48
+}).text("Start").addTo(TitleScreen);
+
+
+var oMainMenuText = new collie.Text({
+    x: "center",
+    y: "center",
+    width: W,
+    textAlign: "center",
+    fontFamily: "Quasi",
+    fontColor: "rgb(64,32,0)",
+    fontSize: 100
+}).text("Main Menu").addTo(MainMenu);
 
 
 collie.Renderer.addLayer(BG);
@@ -123,12 +153,17 @@ collie.Renderer.addLayer(Layer1);
 collie.Renderer.load(document.getElementById("hw"));
 collie.Renderer.start("30fps");
 
-var tTimer = setInterval(timerDone, 1000);
+var tTimer = setInterval(timerDone, 400);
 
 function timerDone() {
     //alert("Timer done.");
     collie.Renderer.removeLayer(Layer1);
     collie.Renderer.addLayer(TitleScreen);
     clearInterval(tTimer);
+}
+
+function showMainMenu(e) {
+    collie.Renderer.removeLayer(TitleScreen);
+    collie.Renderer.addLayer(MainMenu);
 }
 
